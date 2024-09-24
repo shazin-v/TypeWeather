d02c3066b185add9fec7669659159d8c690c7839
Shazin v <shazinv.tech@gmail.com>
2024-09-24 07:26:27 +0530

    package resolve

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-      domains: ['openweathermap.org'], // Add openweathermap.org to allowed domains
-    },
-  };
-  
-  export default nextConfig;
-  
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'openweathermap.org',
+        port: '', 
+        pathname: '/**', 
+      },
+    ],
+  },
+};
+
+export default nextConfig;
